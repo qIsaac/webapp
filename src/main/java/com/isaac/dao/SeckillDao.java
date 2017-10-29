2fb34b470f19d1bdd92bec08a6867bbe561f49d2
isaac <isaacqc@outlook.com>
2017-10-29 21:38:01 +0800

    add procedure

--- src/main/java/com/isaac/dao/SeckillDao.java
+++ src/main/java/com/isaac/dao/SeckillDao.java
@@ -5,6 +5,7 @@ import org.apache.ibatis.annotations.Param;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Isaac on 2017/10/11.
@@ -32,4 +33,10 @@ public interface SeckillDao {
      * @return
      */
     List<Seckill> queryAll (@Param("offset") int offet,@Param("limit") int limit);
+
+    /**
+     * 使用存储过程秒杀
+     * @param paramMap
+     */
+    void killByProcedure(Map<String,Object> paramMap);
 }
